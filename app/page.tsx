79e42c2a7c99a02a5a6c689446300ff266e14b69
wzsamuels <wzsamuels@gmail.com>
2023-05-11 16:30:52 -0400

    Add logo to sign in page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import {getServerSession} from "next-auth";
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import Account from "@/components/Account";
 import SignOutButton from "@/components/SignOutButton";
+import martinLogo from '@/public/martin.png'
+import Image from "next/image";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -11,12 +13,18 @@ export default async function Home() {
 
   if (!session) {
     return (
+      <>
+      <header className='relative'>
+
+      </header>
       <div className='flex flex-col items-center justify-center'>
-        <h1 className='my-8 text-3xl text-center'>Not signed in</h1>
+        <Image src={martinLogo} alt="Martin Logo" width={300} height={300}/>
+        <h1 className='my-8 text-3xl text-center'>Admin Dashboard</h1>
         <div className='flex justify-center'>
           <SignOutButton/>
         </div>
       </div>
+      </>
     )
   }
 
